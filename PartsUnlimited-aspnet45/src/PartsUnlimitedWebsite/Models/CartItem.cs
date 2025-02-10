6b517e23cd37d1e6d5e66b3defd27c9d6849f1de
skalive <srivatsav.kalive@hotmail.com>
2025-02-10 17:04:13 +0000

    Added Comments.

--- PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
+++ PartsUnlimited-aspnet45/src/PartsUnlimitedWebsite/Models/CartItem.cs
@@ -5,7 +5,7 @@ namespace PartsUnlimited.Models
 {
     public class CartItem : ILineItem
     {
-        //My Second change
+        //My Second second change
         [Key]
         public int CartItemId { get; set; }
 
